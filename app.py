a6fda9a232806a1b93d495f97190c1038ebb5e78
Krishna Kumar <141744212+Krishnakumar217@users.noreply.github.com>
2024-03-08 21:39:26 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+notes = []
+@app.route('/',methods=["GET","POST"])
+def index():
+    note = request.form.get("note")
+    if note:
+        notes.append(note)
+    return render_template("code_debugging.html",notes=notes)
+
+if __name__ == '__main__':
+    app.run(debug=True)
